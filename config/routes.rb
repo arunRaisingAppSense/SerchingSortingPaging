009f35bd28b0542caf8be611d0e57c95d70bc76a
Arun Vaishnav <39129274+arunRaisingAppSense@users.noreply.github.com>
2018-05-18 14:38:55 +0530

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,7 @@ Rails.application.routes.draw do
   resources :recipes
   resources :tasks
 
-<<<<<<< HEAD
-  #root to: 'tasks#index'
-=======
 
-
->>>>>>> 4e3137dd5dee76a2032ec13e879914b291118247
   root 'projects#index'
 
 
